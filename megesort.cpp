e3bb708093cbfaf56870b623ffde61002eb81e38
Sanket Khonde <sanketkhonde30@gmail.com>
2022-05-02 14:09:03 +0530

    merge sort algorithm

---
+++ megesort.cpp
@@ -0,0 +1,30 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+
+void merge()
+{
+ vector<int> n1 , n2;
+
+
+}
+
+
+int mergesort()
+{
+    // find mid
+
+    // call for first part
+    mergesort();
+    
+    //call for second part
+    mergesort();
+
+    //merge the the both
+    merge();
+
+}
+int main()
+{
+
+}
