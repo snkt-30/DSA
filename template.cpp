ef28fbd4d8494866515e4c29f03af07f55bdf4f8
Sanket Khonde <sanketkhonde30@gmail.com>
2022-05-02 14:11:06 +0530

    Template for c++ competitive programming

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define nl '\n'
+
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vpi vector<pair<int,int>>
+#define sv set<vector<int>>
+
+
+
+
+
+
+int main()
+{
+    
+}
+
+
